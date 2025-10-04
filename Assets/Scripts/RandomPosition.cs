03208a0305d71411a0a1fb2562904ccf59bae4f0
PenelopeG4m3s <PenelopeG4m3s@gmail.com>
2025-10-04 14:50:04 -0700

    first commit, added all of the project files

---
+++ Assets/Scripts/RandomPosition.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    public KeyCode keyToUsePosition;
+    public float randomXPositionMin;
+    public float randomXPositionMax;
+    public float randomYPositionMin;
+    public float randomYPositionMax;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(keyToUsePosition) )
+        {
+            // TODO change colors
+            transform.position = new Vector3(Random.Range( randomXPositionMin, randomXPositionMax), Random.Range( randomYPositionMin, randomYPositionMax), 0.0f);
+        }
+    }
+}
